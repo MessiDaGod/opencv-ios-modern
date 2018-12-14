MessiDaGod/opencv-ios-modern

commit 1f840ed1c40038d7ad7c1278463832e7bfee371f

removed C API in the following modules: photo, video, imgcodecs, videoio (#13060)
* removed C API in the following modules: photo, video, imgcodecs, videoio

* trying to fix various compile errors and warnings on Windows and Linux

* continue to fix compile errors and warnings

* continue to fix compile errors, warnings, as well as the test failures

* trying to resolve compile warnings on Android

* Update cap_dc1394_v2.cpp

fix warning from the new GCC

modules/videoio/src/cap_images.cpp
@@ -50,6 +50,7 @@
 //
 
 #include "precomp.hpp"
+#include "opencv2/imgcodecs.hpp"
 #include <sys/stat.h>
 
 #ifdef NDEBUG
@@ -62,80 +63,89 @@
 #define _MAX_PATH 1024
 #endif
 
-class CvCapture_Images : public CvCapture
+namespace cv
+{
+
+class CvCapture_Images: public IVideoCapture
 {
 public:
-    CvCapture_Images()
+    void init()
     {
-        filename = NULL;
+        filename.clear();
+        frame.release();
         currentframe = firstframe = 0;
         length = 0;
-        frame = NULL;
         grabbedInOpen = false;
+    }
+    CvCapture_Images()
+    {
+        init();
+    }
+    CvCapture_Images(const String& _filename)
+    {
+        init();
+        open(_filename);
     }
 
     virtual ~CvCapture_Images() CV_OVERRIDE
     {
         close();
     }
-
-    virtual bool open(const char* _filename);
-    virtual void close();
     virtual double getProperty(int) const CV_OVERRIDE;
     virtual bool setProperty(int, double) CV_OVERRIDE;
     virtual bool grabFrame() CV_OVERRIDE;
-    virtual IplImage* retrieveFrame(int) CV_OVERRIDE;
+    virtual bool retrieveFrame(int, OutputArray) CV_OVERRIDE;
+    virtual bool isOpened() const CV_OVERRIDE;
+    virtual int getCaptureDomain() /*const*/ CV_OVERRIDE { return cv::CAP_IMAGES; }
 
-    int getCaptureDomain() /*const*/ CV_OVERRIDE { return cv::CAP_IMAGES; }
+    bool open(const String&);
+    void close();
 protected:
-    char*  filename; // actually a printf-pattern
+
+    std::string filename; // actually a printf-pattern
     unsigned currentframe;
     unsigned firstframe; // number of first frame
     unsigned length; // length of sequence
 
-    IplImage* frame;
+    Mat frame;
     bool grabbedInOpen;
 };
 
-
 void CvCapture_Images::close()
 {
-    if( filename )
-    {
-        free(filename);
-        filename = NULL;
-    }
-    currentframe = firstframe = 0;
-    length = 0;
-    cvReleaseImage( &frame );
+    init();
 }
-
 
 bool CvCapture_Images::grabFrame()
 {
     char str[_MAX_PATH];
-    sprintf(str, filename, firstframe + currentframe);
+
+    if( filename.empty() )
+        return false;
+
+    sprintf(str, filename.c_str(), firstframe + currentframe);
 
     if (grabbedInOpen)
     {
         grabbedInOpen = false;
         ++currentframe;
 
-        return frame != NULL;
+        return !frame.empty();
     }
 
-    cvReleaseImage(&frame);
-    frame = cvLoadImage(str, CV_LOAD_IMAGE_UNCHANGED);
-    if( frame )
+    frame = imread(str, CV_LOAD_IMAGE_UNCHANGED);
+    if( !frame.empty() )
         currentframe++;
 
-    return frame != NULL;
+    return !frame.empty();
 }
 
-IplImage* CvCapture_Images::retrieveFrame(int)
+bool CvCapture_Images::retrieveFrame(int, OutputArray out)
 {
-    return grabbedInOpen ? NULL : frame;
+    frame.copyTo(out);
+    return grabbedInOpen ? false : !frame.empty();
 }
+
 
 double CvCapture_Images::getProperty(int id) const
 {
@@ -151,9 +161,9 @@ double CvCapture_Images::getProperty(int id) const
     case CV_CAP_PROP_POS_AVI_RATIO:
         return (double)currentframe / (double)(length - 1);
     case CV_CAP_PROP_FRAME_WIDTH:
-        return frame ? frame->width : 0;
+        return frame.cols;
     case CV_CAP_PROP_FRAME_HEIGHT:
-        return frame ? frame->height : 0;
+        return frame.rows;
     case CV_CAP_PROP_FPS:
         CV_WARN("collections of images don't have framerates\n");
         return 1;
@@ -199,25 +209,25 @@ bool CvCapture_Images::setProperty(int id, double value)
     return false;
 }
 
-static char* icvExtractPattern(const char *filename, unsigned *offset)
+static std::string extractPattern(const std::string& filename, unsigned& offset)
 {
-    char *name = (char *)filename;
+    std::string name;
 
-    if( !filename )
-        return 0;
+    if( filename.empty() )
+        return std::string();
 
     // check whether this is a valid image sequence filename
-    char *at = strchr(name, '%');
+    char *at = strchr((char*)filename.c_str(), '%');
     if(at)
     {
         unsigned int dummy;
         if(sscanf(at + 1, "%ud", &dummy) != 1)
-            return 0;
-        name = strdup(filename);
+            return std::string();
+        name = filename;
     }
     else // no pattern filename was given - extract the pattern
     {
-        at = name;
+        at = (char*)filename.c_str();
 
         // ignore directory names
         char *slash = strrchr(at, '/');
@@ -231,17 +241,12 @@ static char* icvExtractPattern(const char *filename, unsigned *offset)
         while (*at && !isdigit(*at)) at++;
 
         if(!*at)
-            return 0;
+            return std::string();
 
-        sscanf(at, "%u", offset);
+        sscanf(at, "%u", &offset);
 
-        int size = (int)strlen(filename) + 20;
-        name = (char *)malloc(size);
-        CV_Assert(name != NULL);
-        strncpy(name, filename, at - filename);
-        name[at - filename] = 0;
-
-        strcat(name, "%0");
+        name = filename.substr(0, at - filename.c_str());
+        name += "%0";
 
         int i;
         char *extension;
@@ -250,21 +255,21 @@ static char* icvExtractPattern(const char *filename, unsigned *offset)
         char places[13] = {0};
         sprintf(places, "%dd", i);
 
-        strcat(name, places);
-        strcat(name, extension);
+        name += places;
+        name += extension;
     }
 
     return name;
 }
 
 
-bool CvCapture_Images::open(const char * _filename)
+bool CvCapture_Images::open(const std::string& _filename)
 {
     unsigned offset = 0;
     close();
 
-    filename = icvExtractPattern(_filename, &offset);
-    if(!filename)
+    filename = extractPattern(_filename, offset);
+    if( filename.empty() )
         return false;
 
     // determine the length of the sequence
@@ -272,7 +277,7 @@ bool CvCapture_Images::open(const char * _filename)
     char str[_MAX_PATH];
     for(;;)
     {
-        sprintf(str, filename, offset + length);
+        sprintf(str, filename.c_str(), offset + length);
         struct stat s;
         if(stat(str, &s))
         {
@@ -283,7 +288,7 @@ bool CvCapture_Images::open(const char * _filename)
             }
         }
 
-        if(!cvHaveImageReader(str))
+        if(!haveImageReader(filename))
             break;
 
         length++;
@@ -305,16 +310,19 @@ bool CvCapture_Images::open(const char * _filename)
     return grabRes;
 }
 
-
-CvCapture* cvCreateFileCapture_Images(const char * filename)
+bool CvCapture_Images::isOpened() const
 {
-    CvCapture_Images* capture = new CvCapture_Images;
+    return !filename.empty();
+}
 
-    if( capture->open(filename) )
+Ptr<IVideoCapture> createFileCapture_Images(const String& filename)
+{
+    Ptr<IVideoCapture> capture(new CvCapture_Images(filename));
+    if( capture->isOpened() )
         return capture;
+    return Ptr<IVideoCapture>();
+}
 
-    delete capture;
-    return NULL;
 }
 
 //
@@ -327,7 +335,7 @@ class CvVideoWriter_Images CV_FINAL : public CvVideoWriter
 public:
     CvVideoWriter_Images()
     {
-        filename = 0;
+        filename.clear();
         currentframe = 0;
     }
     virtual ~CvVideoWriter_Images() { close(); }
@@ -339,7 +347,7 @@ public:
 
     int getCaptureDomain() const CV_OVERRIDE { return cv::CAP_IMAGES; }
 protected:
-    char* filename;
+    std::string filename;
     unsigned currentframe;
     std::vector<int> params;
 };
@@ -347,24 +355,21 @@ protected:
 bool CvVideoWriter_Images::writeFrame( const IplImage* image )
 {
     char str[_MAX_PATH];
-    sprintf(str, filename, currentframe);
+    sprintf(str, filename.c_str(), currentframe);
     std::vector<int> image_params = params;
     image_params.push_back(0); // append parameters 'stop' mark
     image_params.push_back(0);
-    int ret = cvSaveImage(str, image, &image_params[0]);
+    cv::Mat img = cv::cvarrToMat(image);
+    bool ret = cv::imwrite(str, img, image_params);
 
     currentframe++;
 
-    return ret > 0;
+    return ret;
 }
 
 void CvVideoWriter_Images::close()
 {
-    if( filename )
-    {
-        free( filename );
-        filename = 0;
-    }
+    filename.clear();
     currentframe = 0;
     params.clear();
 }
@@ -376,13 +381,13 @@ bool CvVideoWriter_Images::open( const char* _filename )
 
     close();
 
-    filename = icvExtractPattern(_filename, &offset);
-    if(!filename)
+    filename = cv::extractPattern(_filename, offset);
+    if(filename.empty())
         return false;
 
     char str[_MAX_PATH];
-    sprintf(str, filename, 0);
-    if(!cvHaveImageWriter(str))
+    sprintf(str, filename.c_str(), 0);
+    if(!cv::haveImageWriter(str))
     {
         close();
         return false;

modules/highgui/include/opencv2/highgui/highgui_c.h
@@ -44,12 +44,6 @@
 
 #include "opencv2/core/core_c.h"
 #include "opencv2/imgproc/imgproc_c.h"
-#ifdef HAVE_OPENCV_IMGCODECS
-#include "opencv2/imgcodecs/imgcodecs_c.h"
-#endif
-#ifdef HAVE_OPENCV_VIDEOIO
-#include "opencv2/videoio/videoio_c.h"
-#endif
 
 #ifdef __cplusplus
 extern "C" {

modules/highgui/src/precomp.hpp
@@ -47,11 +47,11 @@
 #include "opencv2/core/utility.hpp"
 #include "opencv2/core/private.hpp"
 
+#include "opencv2/imgproc.hpp"
 #include "opencv2/imgproc/imgproc_c.h"
 #include "opencv2/highgui/highgui_c.h"
 
 #include "opencv2/imgcodecs.hpp"
-#include "opencv2/imgcodecs/imgcodecs_c.h"
 
 #include <stdlib.h>
 #include <stdio.h>

modules/highgui/src/window_QT.cpp
@@ -2555,8 +2555,10 @@ void DefaultViewPort::updateImage(const CvArr* arr)
     }
 
     nbChannelOriginImage = cvGetElemType(mat);
-
-    cvConvertImage(mat, image2Draw_mat, (origin != 0 ? CV_CVTIMG_FLIP : 0) + CV_CVTIMG_SWAP_RB);
+    CV_Assert(origin == 0);
+    cv::Mat src = cv::cvarrToMat(mat), dst = cv::cvarrToMat(image2Draw_mat);
+    cv::cvtColor(src, dst, cv::COLOR_BGR2RGB, dst.channels());
+    CV_Assert(dst.data == image2Draw_mat->data.ptr);
 
     viewport()->update();
 }
@@ -3002,7 +3004,7 @@ void DefaultViewPort::drawStatusBar()
 
         if (nbChannelOriginImage==CV_8UC1)
         {
-            //all the channel have the same value (because of cvconvertimage), so only the r channel is dsplayed
+            //all the channel have the same value (because of cv::cvtColor(GRAY=>RGB)), so only the r channel is dsplayed
             centralWidget->myStatusBar_msg->setText(tr("<font color='black'>(x=%1, y=%2) ~ </font>")
                 .arg(mouseCoordinate.x())
                 .arg(mouseCoordinate.y())+

modules/highgui/src/window_cocoa.mm
@@ -41,6 +41,7 @@
 //
 //M*/
 #include "precomp.hpp"
+#include "opencv2/imgproc.hpp"
 
 #import <TargetConditionals.h>
 
@@ -910,9 +911,8 @@ static NSSize constrainAspectRatio(NSSize base, NSSize constraint) {
 - (void)setImageData:(CvArr *)arr {
     //cout << "setImageData" << endl;
     NSAutoreleasePool* localpool = [[NSAutoreleasePool alloc] init];
-    CvMat *arrMat, dst, stub;
 
-    arrMat = cvGetMat(arr, &stub);
+    cv::Mat arrMat = cv::cvarrToMat(arr);
     /*CGColorSpaceRef colorspace = NULL;
     CGDataProviderRef provider = NULL;
     int width = cvimage->width;
@@ -933,40 +933,35 @@ static NSSize constrainAspectRatio(NSSize base, NSSize constraint) {
     }*/
 
     NSBitmapImageRep *bitmap = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL
-                pixelsWide:arrMat->cols
-                pixelsHigh:arrMat->rows
+                pixelsWide:arrMat.cols
+                pixelsHigh:arrMat.rows
                 bitsPerSample:8
                 samplesPerPixel:3
                 hasAlpha:NO
                 isPlanar:NO
                 colorSpaceName:NSDeviceRGBColorSpace
                 bitmapFormat: kCGImageAlphaNone
-                bytesPerRow:((arrMat->cols * 3 + 3) & -4)
+                bytesPerRow:((arrMat.cols * 3 + 3) & -4)
                 bitsPerPixel:24];
 
     if (bitmap) {
-        cvInitMatHeader(&dst, arrMat->rows, arrMat->cols, CV_8UC3, [bitmap bitmapData], [bitmap bytesPerRow]);
-        cvConvertImage(arrMat, &dst, CV_CVTIMG_SWAP_RB);
+        cv::Mat dst(arrMat.rows, arrMat.cols, CV_8UC3, [bitmap bitmapData], [bitmap bytesPerRow]);
+        cv::cvtColor(arrMat, dst, cv::COLOR_BGR2RGB);
     }
     else {
         // It's not guaranteed to like the bitsPerPixel:24, but this is a lot slower so we'd rather not do it
         bitmap = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL
-            pixelsWide:arrMat->cols
-            pixelsHigh:arrMat->rows
+            pixelsWide:arrMat.cols
+            pixelsHigh:arrMat.rows
             bitsPerSample:8
             samplesPerPixel:3
             hasAlpha:NO
             isPlanar:NO
             colorSpaceName:NSDeviceRGBColorSpace
-            bytesPerRow:(arrMat->cols * 4)
+            bytesPerRow:(arrMat.cols * 4)
             bitsPerPixel:32];
-        uint8_t *data = [bitmap bitmapData];
-        cvInitMatHeader(&dst, arrMat->rows, arrMat->cols, CV_8UC3, data, (arrMat->cols * 3));
-        cvConvertImage(arrMat, &dst, CV_CVTIMG_SWAP_RB);
-        for (int i = (arrMat->rows * arrMat->cols) - 1; i >= 0; i--) {
-            memmove(data + i * 4, data + i * 3, 3);
-            data[i * 4 + 3] = 0;
-        }
+        cv::Mat dst(arrMat.rows, arrMat.cols, CV_8UC3, [bitmap bitmapData], [bitmap bytesPerRow]);
+        cv::cvtColor(arrMat, dst, cv::COLOR_BGR2RGBA);
     }
 
     if( image ) {

modules/highgui/src/window_gtk.cpp
@@ -140,8 +140,10 @@ void cvImageWidgetSetImage(CvImageWidget * widget, const CvArr *arr){
         widget->original_image = cvCreateMat( mat->rows, mat->cols, CV_8UC3 );
         gtk_widget_queue_resize( GTK_WIDGET( widget ) );
     }
-    cvConvertImage( mat, widget->original_image,
-                            (origin != 0 ? CV_CVTIMG_FLIP : 0) + CV_CVTIMG_SWAP_RB );
+    CV_Assert(origin == 0);
+    cv::Mat src = cv::cvarrToMat(arr), dst = cv::cvarrToMat(widget->original_image);
+    cv::cvtColor(src, dst, cv::COLOR_BGR2RGB, dst.channels());
+    CV_Assert(dst.data == widget->original_image->data.ptr);
     if(widget->scaled_image){
         cvResize( widget->original_image, widget->scaled_image, CV_INTER_AREA );
     }

modules/highgui/src/window_w32.cpp
@@ -1155,7 +1155,7 @@ cvShowImage( const char* name, const CvArr* arr )
     void* dst_ptr = 0;
     const int channels0 = 3;
     int origin = 0;
-    CvMat stub, dst, *image;
+    CvMat stub, *image;
     bool changed_size = false; // philipg
 
     if( !name )
@@ -1209,9 +1209,26 @@ cvShowImage( const char* name, const CvArr* arr )
                                       DIB_RGB_COLORS, &dst_ptr, 0, 0));
     }
 
-    cvInitMatHeader( &dst, size.cy, size.cx, CV_8UC3,
-                     dst_ptr, (size.cx * channels + 3) & -4 );
-    cvConvertImage( image, &dst, origin == 0 ? CV_CVTIMG_FLIP : 0 );
+    {
+        cv::Mat src = cv::cvarrToMat(image);
+        cv::Mat dst(size.cy, size.cx, CV_8UC3, dst_ptr, (size.cx * channels + 3) & -4);
+        if (src.channels() == 1)
+        {
+            cv::cvtColor(src, dst, cv::COLOR_GRAY2BGR);
+            cv::flip(dst, dst, 0);
+        }
+        else if (src.channels() == 4)
+        {
+            cv::cvtColor(src, dst, cv::COLOR_BGRA2BGR);
+            cv::flip(dst, dst, 0);
+        }
+        else
+        {
+            CV_Assert(src.channels() == 3);
+            cv::flip(src, dst, 0);
+        }
+        CV_Assert(dst.data == (uchar*)dst_ptr);
+    }
 
     // ony resize window if needed
     if (changed_size)
@@ -1222,86 +1239,6 @@ cvShowImage( const char* name, const CvArr* arr )
 
     __END__;
 }
-
-#if 0
-CV_IMPL void
-cvShowImageHWND(HWND w_hWnd, const CvArr* arr)
-{
-    CV_FUNCNAME( "cvShowImageHWND" );
-
-    __BEGIN__;
-
-    SIZE size = { 0, 0 };
-    int channels = 0;
-    void* dst_ptr = 0;
-    const int channels0 = 3;
-    int origin = 0;
-    CvMat stub, dst, *image;
-    bool changed_size = false;
-    BITMAPINFO tempbinfo;
-    HDC hdc = NULL;
-
-    if( !arr )
-        EXIT;
-    if( !w_hWnd )
-        EXIT;
-
-    hdc = GetDC(w_hWnd);
-
-    if( CV_IS_IMAGE_HDR( arr ) )
-        origin = ((IplImage*)arr)->origin;
-
-    CV_CALL( image = cvGetMat( arr, &stub ) );
-
-    if ( hdc )
-    {
-            //GetBitmapData
-            BITMAP bmp;
-            GdiFlush();
-            HGDIOBJ h = GetCurrentObject( hdc, OBJ_BITMAP );
-
-            if (h == NULL)
-            EXIT;
-            if (GetObject(h, sizeof(bmp), &bmp) == 0) //GetObject(): returns size of object, 0 if error
-            EXIT;
-
-            channels = bmp.bmBitsPixel/8;
-            dst_ptr = bmp.bmBits;
-     }
-
-    if( size.cx != image->width || size.cy != image->height || channels != channels0 )
-    {
-        changed_size = true;
-
-        uchar buffer[sizeof(BITMAPINFO) + 255*sizeof(RGBQUAD)];
-        BITMAPINFO* binfo = (BITMAPINFO*)buffer;
-
-        BOOL bDeleteObj = DeleteObject(GetCurrentObject(hdc, OBJ_BITMAP));
-                CV_Assert( FALSE != bDeleteObj );
-
-        size.cx = image->width;
-        size.cy = image->height;
-        channels = channels0;
-
-        FillBitmapInfo( binfo, size.cx, size.cy, channels*8, 1 );
-
-        SelectObject( hdc, CreateDIBSection( hdc, binfo, DIB_RGB_COLORS, &dst_ptr, 0, 0));
-    }
-
-    cvInitMatHeader( &dst, size.cy, size.cx, CV_8UC3, dst_ptr, (size.cx * channels + 3) & -4 );
-    cvConvertImage( image, &dst, origin == 0 ? CV_CVTIMG_FLIP : 0 );
-
-    // Image stretching to fit the window
-    RECT rect;
-    GetClientRect(w_hWnd, &rect);
-    StretchDIBits( hdc, 0, 0, rect.right, rect.bottom, 0, 0, image->width, image->height, dst_ptr, &tempbinfo, DIB_RGB_COLORS, SRCCOPY );
-
-    // ony resize window if needed
-    InvalidateRect(w_hWnd, 0, 0);
-
-    __END__;
-}
-#endif
 
 CV_IMPL void cvResizeWindow(const char* name, int width, int height )
 {

modules/imgcodecs/include/opencv2/imgcodecs.hpp
@@ -53,6 +53,52 @@
   @}
 */
 
+/* duplicate of "ImreadModes" enumeration for better compatibility with OpenCV 3.x */
+enum
+{
+/* 8bit, color or not */
+    CV_LOAD_IMAGE_UNCHANGED  =-1,
+/* 8bit, gray */
+    CV_LOAD_IMAGE_GRAYSCALE  =0,
+/* ?, color */
+    CV_LOAD_IMAGE_COLOR      =1,
+/* any depth, ? */
+    CV_LOAD_IMAGE_ANYDEPTH   =2,
+/* ?, any color */
+    CV_LOAD_IMAGE_ANYCOLOR   =4,
+/* ?, no rotate */
+    CV_LOAD_IMAGE_IGNORE_ORIENTATION  =128
+};
+
+/* duplicate of "ImwriteFlags" enumeration for better compatibility with OpenCV 3.x */
+enum
+{
+    CV_IMWRITE_JPEG_QUALITY =1,
+    CV_IMWRITE_JPEG_PROGRESSIVE =2,
+    CV_IMWRITE_JPEG_OPTIMIZE =3,
+    CV_IMWRITE_JPEG_RST_INTERVAL =4,
+    CV_IMWRITE_JPEG_LUMA_QUALITY =5,
+    CV_IMWRITE_JPEG_CHROMA_QUALITY =6,
+    CV_IMWRITE_PNG_COMPRESSION =16,
+    CV_IMWRITE_PNG_STRATEGY =17,
+    CV_IMWRITE_PNG_BILEVEL =18,
+    CV_IMWRITE_PNG_STRATEGY_DEFAULT =0,
+    CV_IMWRITE_PNG_STRATEGY_FILTERED =1,
+    CV_IMWRITE_PNG_STRATEGY_HUFFMAN_ONLY =2,
+    CV_IMWRITE_PNG_STRATEGY_RLE =3,
+    CV_IMWRITE_PNG_STRATEGY_FIXED =4,
+    CV_IMWRITE_PXM_BINARY =32,
+    CV_IMWRITE_EXR_TYPE = 48,
+    CV_IMWRITE_WEBP_QUALITY =64,
+    CV_IMWRITE_PAM_TUPLETYPE = 128,
+    CV_IMWRITE_PAM_FORMAT_NULL = 0,
+    CV_IMWRITE_PAM_FORMAT_BLACKANDWHITE = 1,
+    CV_IMWRITE_PAM_FORMAT_GRAYSCALE = 2,
+    CV_IMWRITE_PAM_FORMAT_GRAYSCALE_ALPHA = 3,
+    CV_IMWRITE_PAM_FORMAT_RGB = 4,
+    CV_IMWRITE_PAM_FORMAT_RGB_ALPHA = 5,
+};
+
 //////////////////////////////// image codec ////////////////////////////////
 namespace cv
 {
@@ -249,6 +295,19 @@ result. See cv::imwrite for the list of supported formats and flags description.
 CV_EXPORTS_W bool imencode( const String& ext, InputArray img,
                             CV_OUT std::vector<uchar>& buf,
                             const std::vector<int>& params = std::vector<int>());
+
+/** @brief Returns true if the specified image can be decoded by OpenCV
+
+@param filename File name of the image
+*/
+CV_EXPORTS_W bool haveImageReader( const String& filename );
+
+/** @brief Returns true if an image with the specified filename can be encoded by OpenCV
+
+ @param filename File name of the image
+ */
+CV_EXPORTS_W bool haveImageWriter( const String& filename );
+
 
 //! @} imgcodecs
 

commit 85580bfde5879f7b759cdc51b4162ed9ededcc4d

videoio: fix CAP_IMAGES
broken in 1f840ed1c40038d7ad7c1278463832e7bfee371f

modules/videoio/src/cap_images.cpp
@@ -288,7 +288,7 @@ bool CvCapture_Images::open(const std::string& _filename)
             }
         }
 
-        if(!haveImageReader(filename))
+        if(!haveImageReader(str))
             break;
 
         length++;