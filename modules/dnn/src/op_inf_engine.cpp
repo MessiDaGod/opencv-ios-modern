opencv/opencv

Add serialize method for IE net wrapper

modules/dnn/src/op_inf_engine.cpp
@@ -233,6 +233,12 @@ const std::string& InfEngineBackendNet::getName() const CV_NOEXCEPT
     return name;
 }
 
+InferenceEngine::StatusCode InfEngineBackendNet::serialize(const std::string&, const std::string&, InferenceEngine::ResponseDesc*) const CV_NOEXCEPT
+{
+    CV_Error(Error::StsNotImplemented, "");
+    return InferenceEngine::StatusCode::OK;
+}
+
 size_t InfEngineBackendNet::layerCount() CV_NOEXCEPT
 {
     return const_cast<const InfEngineBackendNet*>(this)->layerCount();

modules/dnn/src/op_inf_engine.hpp
@@ -68,6 +68,8 @@ public:
 
     virtual InferenceEngine::InputInfo::Ptr getInput(const std::string &inputName) const CV_NOEXCEPT;
 
+    virtual InferenceEngine::StatusCode serialize(const std::string &xmlPath, const std::string &binPath, InferenceEngine::ResponseDesc* resp) const CV_NOEXCEPT;
+
     virtual void getName(char *pName, size_t len) CV_NOEXCEPT;
 
     virtual void getName(char *pName, size_t len) const CV_NOEXCEPT;

MessiDaGod/opencv-ios-modern

backport 7200faab0178bddb1a1c887f8c13625d427bf74c

modules/dnn/src/op_inf_engine.cpp
@@ -235,6 +235,12 @@ const std::string& InfEngineBackendNet::getName() const noexcept
     return name;
 }
 
+InferenceEngine::StatusCode InfEngineBackendNet::serialize(const std::string&, const std::string&, InferenceEngine::ResponseDesc*) const noexcept
+{
+    CV_Error(Error::StsNotImplemented, "");
+    return InferenceEngine::StatusCode::OK;
+}
+
 size_t InfEngineBackendNet::layerCount() noexcept
 {
     return const_cast<const InfEngineBackendNet*>(this)->layerCount();

modules/dnn/src/op_inf_engine.hpp
@@ -69,6 +69,8 @@ public:
 
     virtual InferenceEngine::InputInfo::Ptr getInput(const std::string &inputName) const noexcept;
 
+    virtual InferenceEngine::StatusCode serialize(const std::string &xmlPath, const std::string &binPath, InferenceEngine::ResponseDesc* resp) const noexcept;
+
     virtual void getName(char *pName, size_t len) noexcept;
 
     virtual void getName(char *pName, size_t len) const noexcept;