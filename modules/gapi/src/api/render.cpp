TolyaTalamanov/opencv

Add new render primitives

modules/gapi/src/api/render.cpp
@@ -1,70 +1,50 @@
 #include <opencv2/imgproc.hpp>
-
-#include "opencv2/gapi/render.hpp"
-#include "opencv2/gapi/own/assert.hpp"
+#include <opencv2/gapi/render.hpp>
+#include <opencv2/gapi/own/assert.hpp>
 
 #include "api/render_priv.hpp"
 
-using namespace cv::gapi::wip::draw;
-// FXIME util::visitor ?
-void cv::gapi::wip::draw::render(cv::Mat& bgr, const Prims& prims)
+void cv::gapi::wip::draw::render(cv::Mat &bgr,
+                                 const cv::gapi::wip::draw::Prims &prims,
+                                 const cv::gapi::GKernelPackage& pkg)
 {
-    for (const auto& p : prims)
-    {
-        switch (p.index())
-        {
-            case Prim::index_of<Rect>():
-            {
-                const auto& t_p = cv::util::get<Rect>(p);
-                cv::rectangle(bgr, t_p.rect, t_p.color , t_p.thick, t_p.lt, t_p.shift);
-                break;
-            }
+    cv::GMat in;
+    cv::GArray<Prim> arr;
 
-            case Prim::index_of<Text>():
-            {
-                const auto& t_p = cv::util::get<Text>(p);
-                cv::putText(bgr, t_p.text, t_p.org, t_p.ff, t_p.fs,
-                            t_p.color, t_p.thick, t_p.lt, t_p.bottom_left_origin);
-                break;
-            }
-
-            case Prim::index_of<Circle>():
-            {
-                const auto& c_p = cv::util::get<Circle>(p);
-                cv::circle(bgr, c_p.center, c_p.radius, c_p.color, c_p.thick, c_p.lt, c_p.shift);
-                break;
-            }
-
-            case Prim::index_of<Line>():
-            {
-                const auto& l_p = cv::util::get<Line>(p);
-                cv::line(bgr, l_p.pt1, l_p.pt2, l_p.color, l_p.thick, l_p.lt, l_p.shift);
-                break;
-            }
-
-            default: util::throw_error(std::logic_error("Unsupported draw operation"));
-        }
-    }
+    cv::GComputation comp(cv::GIn(in, arr),
+                          cv::GOut(cv::gapi::wip::draw::GRenderBGR::on(in, arr)));
+    comp.apply(cv::gin(bgr, prims), cv::gout(bgr), cv::compile_args(pkg));
 }
 
-void cv::gapi::wip::draw::render(cv::Mat& y_plane, cv::Mat& uv_plane , const Prims& prims)
+void cv::gapi::wip::draw::render(cv::Mat &y_plane,
+                                 cv::Mat &uv_plane,
+                                 const Prims &prims,
+                                 const GKernelPackage& pkg)
 {
-    cv::Mat bgr;
-    cv::cvtColorTwoPlane(y_plane, uv_plane, bgr, cv::COLOR_YUV2BGR_NV12);
-    render(bgr, prims);
-    BGR2NV12(bgr, y_plane, uv_plane);
+    cv::GMat y_in, uv_in, y_out, uv_out;
+    cv::GArray<Prim> arr;
+    std::tie(y_out, uv_out) = cv::gapi::wip::draw::GRenderNV12::on(y_in, uv_in, arr);
+
+    cv::GComputation comp(cv::GIn(y_in, uv_in, arr), cv::GOut(y_out, uv_out));
+    comp.apply(cv::gin(y_plane, uv_plane, prims),
+               cv::gout(y_plane, uv_plane),
+               cv::compile_args(pkg));
 }
 
-void cv::gapi::wip::draw::splitNV12TwoPlane(const cv::Mat& yuv, cv::Mat& y_plane, cv::Mat& uv_plane) {
-    y_plane.create(yuv.size(),      CV_8UC1);
+void cv::gapi::wip::draw::splitNV12TwoPlane(const cv::Mat& yuv,
+                                                  cv::Mat& y_plane,
+                                                  cv::Mat& uv_plane)
+{
+    y_plane.create(yuv.size(), CV_8UC1);
     uv_plane.create(yuv.size() / 2, CV_8UC2);
 
     // Fill Y plane
     for (int i = 0; i < yuv.rows; ++i)
     {
-        const uchar* in  = yuv.ptr<uchar>(i);
-        uchar* out       = y_plane.ptr<uchar>(i);
-        for (int j = 0; j < yuv.cols; j++) {
+        const uchar *in  = yuv.ptr<uchar>(i);
+        uchar *out = y_plane.ptr<uchar>(i);
+        for (int j = 0; j < yuv.cols; j++)
+        {
             out[j] = in[3 * j];
         }
     }
@@ -72,20 +52,24 @@ void cv::gapi::wip::draw::splitNV12TwoPlane(const cv::Mat& yuv, cv::Mat& y_plane
     // Fill UV plane
     for (int i = 0; i < uv_plane.rows; i++)
     {
-        const uchar* in = yuv.ptr<uchar>(2 * i);
-        uchar* out      = uv_plane.ptr<uchar>(i);
-        for (int j = 0; j < uv_plane.cols; j++) {
-            out[j * 2    ] = in[6 * j + 1];
+        const uchar *in = yuv.ptr<uchar>(2 * i);
+        uchar *out = uv_plane.ptr<uchar>(i);
+        for (int j = 0; j < uv_plane.cols; j++)
+        {
+            out[j * 2] = in[6 * j + 1];
             out[j * 2 + 1] = in[6 * j + 2];
         }
     }
 }
 
-void cv::gapi::wip::draw::BGR2NV12(const cv::Mat& bgr, cv::Mat& y_plane, cv::Mat& uv_plane)
+void cv::gapi::wip::draw::BGR2NV12(const cv::Mat &bgr,
+                                   cv::Mat &y_plane,
+                                   cv::Mat &uv_plane)
 {
     GAPI_Assert(bgr.size().width  % 2 == 0);
     GAPI_Assert(bgr.size().height % 2 == 0);
 
-    cvtColor(bgr, bgr, cv::COLOR_BGR2YUV);
-    splitNV12TwoPlane(bgr, y_plane, uv_plane);
+    cv::Mat cpy;
+    cvtColor(bgr, cpy, cv::COLOR_BGR2YUV);
+    cv::gapi::wip::draw::splitNV12TwoPlane(cpy, y_plane, uv_plane);
 }

modules/gapi/test/common/gapi_render_tests.hpp
@@ -10,14 +10,72 @@
 
 #include "gapi_tests_common.hpp"
 #include "api/render_priv.hpp"
+#include "api/render_ocv.hpp"
+
+// FIXME Add more tests
+#define rect1 Prim{cv::gapi::wip::draw::Rect{cv::Rect{101, 101, 199, 199}, cv::Scalar{153, 172, 58},  1, LINE_8, 0}}
+#define rect2 Prim{cv::gapi::wip::draw::Rect{cv::Rect{100, 100, 199, 199}, cv::Scalar{153, 172, 58},  1, LINE_8, 0}}
+#define rect3 Prim{cv::gapi::wip::draw::Rect{cv::Rect{0  , 0  , 199, 199}, cv::Scalar{153, 172, 58},  1, LINE_8, 0}}
+#define rect8 Prim{cv::gapi::wip::draw::Rect{cv::Rect{100, 100, 200, 200}, cv::Scalar{153, 172, 58},  1, LINE_8, 0}}
+#define box1  Prim{cv::gapi::wip::draw::Rect{cv::Rect{101, 101, 200, 200}, cv::Scalar{153, 172, 58}, -1, LINE_8, 0}}
+#define rects Prims{rect1, rect2, rect3, rect8, box1}
+
+// FIXME OSD segmentation fault width > 0 && height > 0
+#define rect4 Prim{cv::gapi::wip::draw::Rect{cv::Rect{100, 100, 0, 199  }, cv::Scalar{153, 172, 58},  1,  LINE_8, 0}}
+// FIXME OSD segmentation fault x >= 0 && y >= 0
+#define rect5 Prim{cv::gapi::wip::draw::Rect{cv::Rect{0  , -1 , 199, 199}, cv::Scalar{153, 172, 58},  1,  LINE_8, 0}}
+// FIXME if thick > 1 OpenCV cv::rectangle cuts the corner
+#define rect6 Prim{cv::gapi::wip::draw::Rect{cv::Rect{100, 100, 199, 199}, cv::Scalar{153, 172, 58},  10, LINE_8, 0}}
+// FIXME OSD segmentation fault
+#define box2  Prim{cv::gapi::wip::draw::Rect{cv::Rect{100, 100, 199, 199}, cv::Scalar{153, 172, 58},  -1, LINE_8, 0}}
+
+// FIXME Add more tests (Edge cases)
+#define circle1 Prim{cv::gapi::wip::draw::Circle{cv::Point{200, 200}, 100, cv::Scalar{153, 172, 58}, 1, LINE_8, 0}}
+#define circle2 Prim{cv::gapi::wip::draw::Circle{cv::Point{10, 30}  , 2  , cv::Scalar{153, 172, 58}, 1, LINE_8, 0}}
+#define circle3 Prim{cv::gapi::wip::draw::Circle{cv::Point{75, 100} , 50 , cv::Scalar{153, 172, 58}, 5, LINE_8, 0}}
+#define circles Prims{circle1, circle2, circle3}
+
+// FIXME Add more tests (Edge cases)
+#define line1 Prim{cv::gapi::wip::draw::Line{cv::Point{50, 50}, cv::Point{250, 200}, cv::Scalar{153, 172, 58}, 1, LINE_8, 0}}
+#define line2 Prim{cv::gapi::wip::draw::Line{cv::Point{51, 51}, cv::Point{51, 100}, cv::Scalar{153, 172, 58}, 1, LINE_8, 0}}
+#define lines Prims{line1, line2}
+
+// FIXME Add more tests (Edge cases)
+// FIXME accuracy failed
+#define mosaic1 Prim{cv::gapi::wip::draw::Mosaic{cv::Rect{100, 100, 200, 200}, 5, 0}}
+// FIXME accuracy failed
+#define mosaics Prims{mosaic1}
+
+#define image1 Prim{cv::gapi::wip::draw::Image{100, 100, getImage(), getAlpha()}}
+#define images Prims{image1}
+
+// FIXME Add more tests (Edge cases)
+#define polygon1 Prim{cv::gapi::wip::draw::Poly{ {cv::Point{100, 100}, cv::Point{50, 200}, cv::Point{200, 30}, cv::Point{150, 50} }, cv::Scalar{153, 172, 58}, 1, LINE_8, 0} }
+#define polygons Prims{polygon1}
+
+// FIXME Add more tests (Edge cases)
+#define text1 Prim{cv::gapi::wip::draw::Text{"TheBrownFoxJump", cv::Point{100, 100}, FONT_HERSHEY_SIMPLEX, 2, cv::Scalar{102, 178, 240}, 1, LINE_8, false} }
+#define texts Prims{text1}
 
 namespace opencv_test
 {
 
-using Points            = std::vector<cv::Point>;
-using Rects             = std::vector<cv::Rect>;
-using PairOfPoints      = std::pair<cv::Point, cv::Point>;
-using VecOfPairOfPoints = std::vector<PairOfPoints>;
+using Prims = cv::gapi::wip::draw::Prims;
+using Prim  = cv::gapi::wip::draw::Prim;
+
+namespace
+{
+    // FIXME avoid this
+    cv::Mat getImage()
+    {
+        return cv::Mat(cv::Size(200, 200), CV_8UC3, cv::Scalar::all(255));
+    }
+
+    cv::Mat getAlpha()
+    {
+        return cv::Mat(cv::Size(200, 200), CV_8UC3, cv::Scalar::all(1));
+    }
+}
 
 template<class T>
 class RenderWithParam : public TestWithParam<T>
@@ -26,47 +84,36 @@ protected:
     void Init()
     {
         MatType type = CV_8UC3;
-        out_mat_ocv  = cv::Mat(sz, type, cv::Scalar(255));
-        out_mat_gapi = cv::Mat(sz, type, cv::Scalar(255));
-
-        if (isNV12Format) {
-            /* NB: When converting data from BGR to NV12, data loss occurs,
-             * so the reference data is subjected to the same transformation
-             * for correct comparison of the test results */
-            cv::gapi::wip::draw::BGR2NV12(out_mat_ocv, y, uv);
-            cv::cvtColorTwoPlane(y, uv, out_mat_ocv, cv::COLOR_YUV2BGR_NV12);
-        }
-    }
-
-    void Run()
-    {
-        if (isNV12Format) {
-            cv::gapi::wip::draw::BGR2NV12(out_mat_gapi, y, uv);
-            cv::gapi::wip::draw::render(y, uv, prims);
-            cv::cvtColorTwoPlane(y, uv, out_mat_gapi, cv::COLOR_YUV2BGR_NV12);
-
-            // NB: Also due to data loss
-            cv::gapi::wip::draw::BGR2NV12(out_mat_ocv, y, uv);
-            cv::cvtColorTwoPlane(y, uv, out_mat_ocv, cv::COLOR_YUV2BGR_NV12);
-        } else {
-            cv::gapi::wip::draw::render(out_mat_gapi, prims);
-        }
+        mat_ocv.create(sz, type);
+        mat_gapi.create(sz, type);
+        cv::randu(mat_ocv, cv::Scalar::all(0), cv::Scalar::all(255));
+        mat_ocv.copyTo(mat_gapi);
     }
 
     cv::Size sz;
-    cv::Scalar color;
-    int thick;
-    int lt;
-    bool isNV12Format;
     std::vector<cv::gapi::wip::draw::Prim> prims;
-    cv::Mat y, uv;
-    cv::Mat out_mat_ocv, out_mat_gapi;
+    cv::gapi::GKernelPackage pkg;
+
+    cv::Mat y_mat_ocv, uv_mat_ocv, y_mat_gapi, uv_mat_gapi, mat_ocv, mat_gapi;
 };
 
-struct RenderTextTest   : public RenderWithParam <std::tuple<cv::Size,std::string,Points,int,double,cv::Scalar,int,int,bool,bool>> {};
-struct RenderRectTest   : public RenderWithParam <std::tuple<cv::Size,Rects,cv::Scalar,int,int,int,bool>>                          {};
-struct RenderCircleTest : public RenderWithParam <std::tuple<cv::Size,Points,int,cv::Scalar,int,int,int,bool>>                     {};
-struct RenderLineTest   : public RenderWithParam <std::tuple<cv::Size,VecOfPairOfPoints,cv::Scalar,int,int,int,bool>>              {};
+struct RenderNV12 : public RenderWithParam <std::tuple<cv::Size,cv::gapi::wip::draw::Prims,cv::gapi::GKernelPackage>>
+{
+    void ComputeRef()
+    {
+        cv::cvtColor(mat_ocv, mat_ocv, cv::COLOR_BGR2YUV);
+        cv::gapi::wip::draw::drawPrimitivesOCVYUV(mat_ocv, prims);
+        cv::gapi::wip::draw::splitNV12TwoPlane(mat_ocv, y_mat_ocv, uv_mat_ocv);
+    }
+};
+
+struct RenderBGR : public RenderWithParam <std::tuple<cv::Size,cv::gapi::wip::draw::Prims,cv::gapi::GKernelPackage>>
+{
+    void ComputeRef()
+    {
+        cv::gapi::wip::draw::drawPrimitivesOCVBGR(mat_ocv, prims);
+    }
+};
 
 } // opencv_test
 

MessiDaGod/opencv-ios-modern

modules/gapi/src/api/render.cpp
@@ -1,91 +1,50 @@
 #include <opencv2/imgproc.hpp>
-
-#include "opencv2/gapi/render.hpp"
-#include "opencv2/gapi/own/assert.hpp"
+#include <opencv2/gapi/render.hpp>
+#include <opencv2/gapi/own/assert.hpp>
 
 #include "api/render_priv.hpp"
 
-using namespace cv::gapi::wip::draw;
-// FXIME util::visitor ?
-void cv::gapi::wip::draw::render(cv::Mat& bgr, const Prims& prims)
+void cv::gapi::wip::draw::render(cv::Mat &bgr,
+                                 const cv::gapi::wip::draw::Prims &prims,
+                                 const cv::gapi::GKernelPackage& pkg)
 {
-    for (const auto& p : prims)
-    {
-        switch (p.index())
-        {
-            case Prim::index_of<Rect>():
-            {
-                const auto& t_p = cv::util::get<Rect>(p);
-                cv::rectangle(bgr, t_p.rect, t_p.color , t_p.thick, t_p.lt, t_p.shift);
-                break;
-            }
+    cv::GMat in;
+    cv::GArray<Prim> arr;
 
-            case Prim::index_of<Text>():
-            {
-                const auto& t_p = cv::util::get<Text>(p);
-                cv::putText(bgr, t_p.text, t_p.org, t_p.ff, t_p.fs,
-                            t_p.color, t_p.thick, t_p.lt, t_p.bottom_left_origin);
-                break;
-            }
-
-            case Prim::index_of<Circle>():
-            {
-                const auto& c_p = cv::util::get<Circle>(p);
-                cv::circle(bgr, c_p.center, c_p.radius, c_p.color, c_p.thick, c_p.lt, c_p.shift);
-                break;
-            }
-
-            case Prim::index_of<Line>():
-            {
-                const auto& l_p = cv::util::get<Line>(p);
-                cv::line(bgr, l_p.pt1, l_p.pt2, l_p.color, l_p.thick, l_p.lt, l_p.shift);
-                break;
-            }
-
-            default: util::throw_error(std::logic_error("Unsupported draw operation"));
-        }
-    }
+    cv::GComputation comp(cv::GIn(in, arr),
+                          cv::GOut(cv::gapi::wip::draw::GRenderBGR::on(in, arr)));
+    comp.apply(cv::gin(bgr, prims), cv::gout(bgr), cv::compile_args(pkg));
 }
 
-void cv::gapi::wip::draw::render(cv::Mat& y_plane, cv::Mat& uv_plane , const Prims& prims)
+void cv::gapi::wip::draw::render(cv::Mat &y_plane,
+                                 cv::Mat &uv_plane,
+                                 const Prims &prims,
+                                 const GKernelPackage& pkg)
 {
-    cv::Mat bgr;
-    cv::cvtColorTwoPlane(y_plane, uv_plane, bgr, cv::COLOR_YUV2BGR_NV12);
-    render(bgr, prims);
-    BGR2NV12(bgr, y_plane, uv_plane);
+    cv::GMat y_in, uv_in, y_out, uv_out;
+    cv::GArray<Prim> arr;
+    std::tie(y_out, uv_out) = cv::gapi::wip::draw::GRenderNV12::on(y_in, uv_in, arr);
+
+    cv::GComputation comp(cv::GIn(y_in, uv_in, arr), cv::GOut(y_out, uv_out));
+    comp.apply(cv::gin(y_plane, uv_plane, prims),
+               cv::gout(y_plane, uv_plane),
+               cv::compile_args(pkg));
 }
 
-void cv::gapi::wip::draw::splitNV12TwoPlane(const cv::Mat& yuv, cv::Mat& y_plane, cv::Mat& uv_plane) {
-    y_plane.create(yuv.size(),      CV_8UC1);
-    uv_plane.create(yuv.size() / 2, CV_8UC2);
-
-    // Fill Y plane
-    for (int i = 0; i < yuv.rows; ++i)
-    {
-        const uchar* in  = yuv.ptr<uchar>(i);
-        uchar* out       = y_plane.ptr<uchar>(i);
-        for (int j = 0; j < yuv.cols; j++) {
-            out[j] = in[3 * j];
-        }
-    }
-
-    // Fill UV plane
-    for (int i = 0; i < uv_plane.rows; i++)
-    {
-        const uchar* in = yuv.ptr<uchar>(2 * i);
-        uchar* out      = uv_plane.ptr<uchar>(i);
-        for (int j = 0; j < uv_plane.cols; j++) {
-            out[j * 2    ] = in[6 * j + 1];
-            out[j * 2 + 1] = in[6 * j + 2];
-        }
-    }
-}
-
-void cv::gapi::wip::draw::BGR2NV12(const cv::Mat& bgr, cv::Mat& y_plane, cv::Mat& uv_plane)
+void cv::gapi::wip::draw::BGR2NV12(const cv::Mat &bgr,
+                                   cv::Mat &y_plane,
+                                   cv::Mat &uv_plane)
 {
     GAPI_Assert(bgr.size().width  % 2 == 0);
     GAPI_Assert(bgr.size().height % 2 == 0);
 
-    cvtColor(bgr, bgr, cv::COLOR_BGR2YUV);
-    splitNV12TwoPlane(bgr, y_plane, uv_plane);
+    cv::Mat yuv;
+    cvtColor(bgr, yuv, cv::COLOR_BGR2YUV);
+
+    std::vector<cv::Mat> chs(3);
+    cv::split(yuv, chs);
+    y_plane = chs[0];
+
+    cv::merge(std::vector<cv::Mat>{chs[1], chs[2]}, uv_plane);
+    cv::resize(uv_plane, uv_plane, uv_plane.size() / 2, cv::INTER_LINEAR);
 }

modules/gapi/test/common/gapi_render_tests.hpp
@@ -10,14 +10,53 @@
 
 #include "gapi_tests_common.hpp"
 #include "api/render_priv.hpp"
+#include "api/render_ocv.hpp"
+
+#define rect1 Prim{cv::gapi::wip::draw::Rect{cv::Rect{101, 101, 199, 199}, cv::Scalar{153, 172, 58},  1, LINE_8, 0}}
+#define rect2 Prim{cv::gapi::wip::draw::Rect{cv::Rect{100, 100, 199, 199}, cv::Scalar{153, 172, 58},  1, LINE_8, 0}}
+#define rect3 Prim{cv::gapi::wip::draw::Rect{cv::Rect{0  , 0  , 199, 199}, cv::Scalar{153, 172, 58},  1, LINE_8, 0}}
+#define rect4 Prim{cv::gapi::wip::draw::Rect{cv::Rect{100, 100, 0, 199  }, cv::Scalar{153, 172, 58},  1,  LINE_8, 0}}
+#define rect5 Prim{cv::gapi::wip::draw::Rect{cv::Rect{0  , -1 , 199, 199}, cv::Scalar{153, 172, 58},  1,  LINE_8, 0}}
+#define rect6 Prim{cv::gapi::wip::draw::Rect{cv::Rect{100, 100, 199, 199}, cv::Scalar{153, 172, 58},  10, LINE_8, 0}}
+#define rect7 Prim{cv::gapi::wip::draw::Rect{cv::Rect{100, 100, 200, 200}, cv::Scalar{153, 172, 58},  1, LINE_8, 0}}
+#define box1  Prim{cv::gapi::wip::draw::Rect{cv::Rect{101, 101, 200, 200}, cv::Scalar{153, 172, 58}, -1, LINE_8, 0}}
+#define box2  Prim{cv::gapi::wip::draw::Rect{cv::Rect{100, 100, 199, 199}, cv::Scalar{153, 172, 58},  -1, LINE_8, 0}}
+#define rects Prims{rect1, rect2, rect3, rect4, rect5, rect6, rect7, box1, box2}
+
+#define circle1 Prim{cv::gapi::wip::draw::Circle{cv::Point{200, 200}, 100, cv::Scalar{153, 172, 58}, 1, LINE_8, 0}}
+#define circle2 Prim{cv::gapi::wip::draw::Circle{cv::Point{10, 30}  , 2  , cv::Scalar{153, 172, 58}, 1, LINE_8, 0}}
+#define circle3 Prim{cv::gapi::wip::draw::Circle{cv::Point{75, 100} , 50 , cv::Scalar{153, 172, 58}, 5, LINE_8, 0}}
+#define circles Prims{circle1, circle2, circle3}
+
+#define line1 Prim{cv::gapi::wip::draw::Line{cv::Point{50, 50}, cv::Point{250, 200}, cv::Scalar{153, 172, 58}, 1, LINE_8, 0}}
+#define line2 Prim{cv::gapi::wip::draw::Line{cv::Point{51, 51}, cv::Point{51, 100}, cv::Scalar{153, 172, 58}, 1, LINE_8, 0}}
+#define lines Prims{line1, line2}
+
+#define mosaic1 Prim{cv::gapi::wip::draw::Mosaic{cv::Rect{100, 100, 200, 200}, 5, 0}}
+#define mosaics Prims{mosaic1}
+
+#define image1 Prim{cv::gapi::wip::draw::Image{cv::Point(100, 100), cv::Mat(cv::Size(200, 200), CV_8UC3, cv::Scalar::all(255)),\
+                                                                    cv::Mat(cv::Size(200, 200), CV_32FC1, cv::Scalar::all(1))}}
+
+#define image2 Prim{cv::gapi::wip::draw::Image{cv::Point(100, 100), cv::Mat(cv::Size(200, 200), CV_8UC3, cv::Scalar::all(255)),\
+                                                                    cv::Mat(cv::Size(200, 200), CV_32FC1, cv::Scalar::all(0.5))}}
+
+#define image3 Prim{cv::gapi::wip::draw::Image{cv::Point(100, 100), cv::Mat(cv::Size(200, 200), CV_8UC3, cv::Scalar::all(255)),\
+                                                                    cv::Mat(cv::Size(200, 200), CV_32FC1, cv::Scalar::all(0.0))}}
+
+#define images Prims{image1, image2, image3}
+
+#define polygon1 Prim{cv::gapi::wip::draw::Poly{ {cv::Point{100, 100}, cv::Point{50, 200}, cv::Point{200, 30}, cv::Point{150, 50} }, cv::Scalar{153, 172, 58}, 1, LINE_8, 0} }
+#define polygons Prims{polygon1}
+
+#define text1 Prim{cv::gapi::wip::draw::Text{"TheBrownFoxJump", cv::Point{100, 100}, FONT_HERSHEY_SIMPLEX, 2, cv::Scalar{102, 178, 240}, 1, LINE_8, false} }
+#define texts Prims{text1}
 
 namespace opencv_test
 {
 
-using Points            = std::vector<cv::Point>;
-using Rects             = std::vector<cv::Rect>;
-using PairOfPoints      = std::pair<cv::Point, cv::Point>;
-using VecOfPairOfPoints = std::vector<PairOfPoints>;
+using Prims = cv::gapi::wip::draw::Prims;
+using Prim  = cv::gapi::wip::draw::Prim;
 
 template<class T>
 class RenderWithParam : public TestWithParam<T>
@@ -26,47 +65,50 @@ protected:
     void Init()
     {
         MatType type = CV_8UC3;
-        out_mat_ocv  = cv::Mat(sz, type, cv::Scalar(255));
-        out_mat_gapi = cv::Mat(sz, type, cv::Scalar(255));
-
-        if (isNV12Format) {
-            /* NB: When converting data from BGR to NV12, data loss occurs,
-             * so the reference data is subjected to the same transformation
-             * for correct comparison of the test results */
-            cv::gapi::wip::draw::BGR2NV12(out_mat_ocv, y, uv);
-            cv::cvtColorTwoPlane(y, uv, out_mat_ocv, cv::COLOR_YUV2BGR_NV12);
-        }
-    }
-
-    void Run()
-    {
-        if (isNV12Format) {
-            cv::gapi::wip::draw::BGR2NV12(out_mat_gapi, y, uv);
-            cv::gapi::wip::draw::render(y, uv, prims);
-            cv::cvtColorTwoPlane(y, uv, out_mat_gapi, cv::COLOR_YUV2BGR_NV12);
-
-            // NB: Also due to data loss
-            cv::gapi::wip::draw::BGR2NV12(out_mat_ocv, y, uv);
-            cv::cvtColorTwoPlane(y, uv, out_mat_ocv, cv::COLOR_YUV2BGR_NV12);
-        } else {
-            cv::gapi::wip::draw::render(out_mat_gapi, prims);
-        }
+        mat_ocv.create(sz, type);
+        mat_gapi.create(sz, type);
+        cv::randu(mat_ocv, cv::Scalar::all(0), cv::Scalar::all(255));
+        mat_ocv.copyTo(mat_gapi);
     }
 
     cv::Size sz;
-    cv::Scalar color;
-    int thick;
-    int lt;
-    bool isNV12Format;
     std::vector<cv::gapi::wip::draw::Prim> prims;
-    cv::Mat y, uv;
-    cv::Mat out_mat_ocv, out_mat_gapi;
+    cv::gapi::GKernelPackage pkg;
+
+    cv::Mat y_mat_ocv, uv_mat_ocv, y_mat_gapi, uv_mat_gapi, mat_ocv, mat_gapi;
 };
 
-struct RenderTextTest   : public RenderWithParam <std::tuple<cv::Size,std::string,Points,int,double,cv::Scalar,int,int,bool,bool>> {};
-struct RenderRectTest   : public RenderWithParam <std::tuple<cv::Size,Rects,cv::Scalar,int,int,int,bool>>                          {};
-struct RenderCircleTest : public RenderWithParam <std::tuple<cv::Size,Points,int,cv::Scalar,int,int,int,bool>>                     {};
-struct RenderLineTest   : public RenderWithParam <std::tuple<cv::Size,VecOfPairOfPoints,cv::Scalar,int,int,int,bool>>              {};
+using TestArgs = std::tuple<cv::Size,cv::gapi::wip::draw::Prims,cv::gapi::GKernelPackage>;
+
+struct RenderNV12 : public RenderWithParam<TestArgs>
+{
+    void ComputeRef()
+    {
+        cv::gapi::wip::draw::BGR2NV12(mat_ocv, y_mat_ocv, uv_mat_ocv);
+
+        // NV12 -> YUV
+        cv::Mat upsample_uv, yuv;
+        cv::resize(uv_mat_ocv, upsample_uv, uv_mat_ocv.size() * 2, cv::INTER_LINEAR);
+        cv::merge(std::vector<cv::Mat>{y_mat_ocv, upsample_uv}, yuv);
+
+        cv::gapi::wip::draw::drawPrimitivesOCVYUV(yuv, prims);
+
+        // YUV -> NV12
+        std::vector<cv::Mat> chs(3);
+        cv::split(yuv, chs);
+        cv::merge(std::vector<cv::Mat>{chs[1], chs[2]}, uv_mat_ocv);
+        y_mat_ocv = chs[0];
+        cv::resize(uv_mat_ocv, uv_mat_ocv, uv_mat_ocv.size() / 2, cv::INTER_LINEAR);
+    }
+};
+
+struct RenderBGR : public RenderWithParam<TestArgs>
+{
+    void ComputeRef()
+    {
+        cv::gapi::wip::draw::drawPrimitivesOCVBGR(mat_ocv, prims);
+    }
+};
 
 } // opencv_test
 